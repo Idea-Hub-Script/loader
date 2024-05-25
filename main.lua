3fb004eaf547731137ca8e6b72b257faac62d25d
ro-ideahub <ideahub@brettrm001.com>
2024-05-24 23:20:09 -0400

    Add Army Tycoon Support

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
 if game.PlaceId == 10851599 then -- Roblox Talent Show
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Idea-Hub-Script/roblox-talent-show/main/main.lua"))()
+elseif game.PlaceId == 4325400580 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Idea-Hub-Script/army-tycoon/main/main.lua"))()
 else
     game.StarterGui:SetCore("SendNotification", {
         Title = "Idea Hub | Unsupported Game",
