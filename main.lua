102659629124608ff78c9b6ef00a900f4287f80f
ro-ideahub <ideahub@brettrm001.com>
2024-06-01 18:16:27 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 if game.PlaceId == 10851599 then -- Roblox Talent Show
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Idea-Hub-Script/roblox-talent-show/main/main.lua"))()
-elseif game.PlaceId == 4325400580 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/50da22453dd71bc9faf29080bcd141ef.lua"))()
+elseif game.PlaceId == 4325400580 then -- Army Tycoon
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Idea-Hub-Script/army-tycoon/main/main.lua"))()
 else
     game.StarterGui:SetCore("SendNotification", {
