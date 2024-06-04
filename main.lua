29c17a8ffa205711dc697e69944f23b09f93f0a4
ro-ideahub <ideahub@brettrm001.com>
2024-06-04 14:44:20 -0400

    Add greenville

--- main.lua
+++ main.lua
@@ -2,6 +2,8 @@ if game.PlaceId == 10851599 then -- Roblox Talent Show
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/50da22453dd71bc9faf29080bcd141ef.lua"))()
 elseif game.PlaceId == 4325400580 then -- Army Tycoon
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Idea-Hub-Script/army-tycoon/main/main.lua"))()
+elseif game.PlaceId == 891852901 then -- Greenville
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b6348a080de97ba310076d44efa718c.lua"))()
 else
     game.StarterGui:SetCore("SendNotification", {
         Title = "Idea Hub | Unsupported Game",
