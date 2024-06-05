a35b3783d6725dded878e3d6830933f4717316c7
ro-ideahub <ideahub@brettrm001.com>
2024-06-05 13:21:00 -0400

    Update Army Tycoon

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 if game.PlaceId == 10851599 then -- Roblox Talent Show
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/50da22453dd71bc9faf29080bcd141ef.lua"))()
 elseif game.PlaceId == 4325400580 then -- Army Tycoon
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Idea-Hub-Script/army-tycoon/main/main.lua"))()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/51d0b7126d6f97014024141216a08bc1.lua"))()
 elseif game.PlaceId == 891852901 then -- Greenville
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b6348a080de97ba310076d44efa718c.lua"))()
 else
